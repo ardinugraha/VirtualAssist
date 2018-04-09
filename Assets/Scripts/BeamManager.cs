22b814f44e81227ab97e80bdcd9a678b62acd284
ardi nugraha <ardinugrahaardi@gmail.com>
2018-04-09 17:04:04 +0700

    2ndCommit

--- Assets/Scripts/BeamManager.cs
+++ Assets/Scripts/BeamManager.cs
@@ -11,7 +11,7 @@ public class BeamManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         sceneController = GameObject.Find("SceneController").GetComponent<SceneController>();
-        model
+        
 	}
 	
 	// Update is called once per frame
@@ -24,18 +24,5 @@ public class BeamManager : MonoBehaviour {
     {
         GameObject model = SceneController.Instance.Model;
         ModelController modelController = model.GetComponent<ModelController>();
-
-        if (runAsStandAloneOrMPasMaster && SceneController.Instance.GameStage != GameStage.LobbyMenuStage &&
-            modelController.BeamMode == BeamState.Active)
-        {
-            modelController.BeamMode = BeamState.Inactive;
-            modelController.Show(true);
-            model.transform.position = DefaultCursorManager.Instance.transform.position;
-
-            DefaultCursorManager.Instance.SetCursorIconToDefault(true);
-
-            // Broadcast Object Position over Network 
-            RemoteHeadManager.Instance.BeamObjectPosition(model);
-        }
     }
 }
--- Assets/Scripts/ModelController.cs
+++ Assets/Scripts/ModelController.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using VA.App.Scripts;
 
-public class ModelController : MonoBehaviour {
+public class ModelController : MonoBehaviour , IInputClickHandler,IFocusable {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public BeamState beamState;
+    private GameObject defaultCursor;
+
+    public void OnFocusEnter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnFocusExit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+        defaultCursor = GameObject.Find("DefaultCursor");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if(beamState == BeamState.Active)
+        {
+            transform.position = defaultCursor.transform.position;
+        }
 	}
 }
