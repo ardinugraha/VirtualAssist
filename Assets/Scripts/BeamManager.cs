f806d1b5b475e5b93ea46c90cecf75965e005765
ardi nugraha <ardinugrahaardi@gmail.com>
2018-04-09 14:36:50 +0700

    First commit yay

---
+++ Assets/Scripts/BeamManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Sharing;
+using HoloToolkit.Unity;
+
+public class BeamManager : MonoBehaviour {
+
+    private SceneController sceneController;
+	// Use this for initialization
+	void Start () {
+        sceneController = GameObject.Find("SceneController").GetComponent<SceneController>();
+        model
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        GameObject model = SceneController.Instance.Model;
+        ModelController modelController = model.GetComponent<ModelController>();
+
+        if (runAsStandAloneOrMPasMaster && SceneController.Instance.GameStage != GameStage.LobbyMenuStage &&
+            modelController.BeamMode == BeamState.Active)
+        {
+            modelController.BeamMode = BeamState.Inactive;
+            modelController.Show(true);
+            model.transform.position = DefaultCursorManager.Instance.transform.position;
+
+            DefaultCursorManager.Instance.SetCursorIconToDefault(true);
+
+            // Broadcast Object Position over Network 
+            RemoteHeadManager.Instance.BeamObjectPosition(model);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine.XR.WSA;
+using HoloToolkit.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq; //.Core;
+using UnityEngine.UI;
+using HoloToolkit.Sharing;
+
+public class SceneController : Singleton<SceneController>
+{
+
+    private GameObject DefaultCursor;
+    public GameObject Model;
+    
+	// Use this for initialization
+	void Start () {
+        Model = GameObject.Find("Model");
+        Model.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
